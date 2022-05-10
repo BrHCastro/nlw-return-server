f8c425288091794cd703fe74a16e96b6201a9bca
BrHCastro <hendecastro@gmail.com>
2022-05-09 21:31:32 -0300

    added port env variable

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import { routes } from './routes';
+import express from "express";
+import cors from "cors";
+import { routes } from "./routes";
 
 const app = express();
 
@@ -8,4 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('HTTP server started on port 3333 🚀'));
+app.listen(process.env.PORT || 3333, () =>
+  console.log("HTTP server started on port 3333 🚀")
+);
